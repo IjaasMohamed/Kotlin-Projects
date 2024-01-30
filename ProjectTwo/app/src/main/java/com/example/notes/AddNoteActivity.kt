a9618d8a2a58b8d2699119491ba599045e396c81
Ijaas Fouzer <96341377+IjaasMohamed@users.noreply.github.com>
2024-01-29 23:01:29 -0500

    database config

--- ProjectTwo/app/src/main/java/com/example/notes/AddNoteActivity.kt
+++ ProjectTwo/app/src/main/java/com/example/notes/AddNoteActivity.kt
@@ -2,10 +2,19 @@ package com.example.notes
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.notes.databinding.ActivityAddNoteBinding
 
 class AddNoteActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityAddNoteBinding
+    private lateinit var db : NotesDatabaseHelper
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_add_note)
+        binding = ActivityAddNoteBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        db = NotesDatabaseHelper(this)
+
     }
 }

b48e63f3158491c1ec0e1fe5fa729e1b62214e31
Ijaas Fouzer <96341377+IjaasMohamed@users.noreply.github.com>
2024-02-01 03:08:01 -0500

    data class created

--- ProjectThree/app/src/main/java/com/example/MusicApp/MainActivity.kt
+++ ProjectThree/app/src/main/java/com/example/MusicApp/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.MusicApp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.shoponline.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {

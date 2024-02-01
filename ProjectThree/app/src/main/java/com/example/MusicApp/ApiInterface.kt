ce4ca3d6bdf79297d6cb21a8ff43977618c5d67d
Ijaas Fouzer <96341377+IjaasMohamed@users.noreply.github.com>
2024-02-01 03:31:05 -0500

    Api Interface Configured

---
+++ ProjectThree/app/src/main/java/com/example/MusicApp/ApiInterface.kt
@@ -0,0 +1,14 @@
+package com.example.MusicApp
+
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Headers
+import retrofit2.http.Query
+
+interface ApiInterface {
+
+    @Headers("X-RapidAPI-Key: 3625ca21fdmshe9cb9782e95e085p1b4867jsn0f1845a34e3d",
+             "X-RapidAPI-Host: deezerdevs-deezer.p.rapidapi.com")
+    @GET("search")
+    fun getData(@Query("q") query: String) : Call<List<MyData>>
+}
